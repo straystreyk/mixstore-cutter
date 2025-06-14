44e6dd1a20a64f002fede84958fb4c15d53548ca
Tigran <tskanyan@nanosemantics.ai>
2025-06-14 22:46:04 +0300

    updates

--- app/src/main/java/com/example/testandroidapp/data/api/ApiService.kt
+++ app/src/main/java/com/example/testandroidapp/data/api/ApiService.kt
@@ -3,8 +3,12 @@ package com.example.testandroidapp.data.api
 
 import com.example.testandroidapp.data.model.UserDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface ApiService {
     @GET("users")
     suspend fun getUsers(): List<UserDto>
+
+    @GET("users/{id}")
+    suspend fun getUserById(@Path("id") id: Long): UserDto
 }
--- app/src/main/java/com/example/testandroidapp/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/testandroidapp/data/repository/UserRepository.kt
@@ -7,4 +7,8 @@ class UserRepository {
     suspend fun getUsers(): List<UserDto> {
         return RetrofitInstance.api.getUsers()
     }
+
+    suspend fun getUserById(id: Long): UserDto {
+        return RetrofitInstance.api.getUserById(id)
+    }
 }
